52122befdb9b37d32ec3b058e582af8011d283e1
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-06 02:55:59 +0300

    created login function that will check if token exits thats will return new message loged in if not check username and get password and comapre with password thats come from clint side if valid thats will return success. relates #20

---
+++ server/controllers/login.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { compare } = require('bcrypt');
+const { loginValidationSchema } = require('../validation');
+const { validate } = require('../validation');
+const { GenericError, AuthHelpers } = require('../helpers');
+const { userPassword } = require('../database/queries');
+
+const login = (req, res) => {
+  const isToken = req.cookies.token;
+  if (isToken) {
+    res.json({ message: 'loged in' });
+    return;
+  }
+  const { username, password } = req.body;
+  let user;
+  validate(loginValidationSchema, { username, password })
+    .then(() => userPassword(username))
+    .then((data) => {
+      user = data.rows[0];
+      if (!user) {
+        throw new GenericError(400, 'Please double check your password and username');
+      }
+      return compare(password, user.password);
+    }).then((isPasswordMatched) => {
+      if (!isPasswordMatched) {
+        throw new GenericError(400, 'Please double check your password and username');
+      }
+      return AuthHelpers.genearteToken({ id: user.id });
+    })
+    .then((jwt) => res.cookie('token', jwt, { httpOnly: true }).json({ message: 'success' }))
+    .catch((error) => res.status(error.error || 500).json({ error: error.msg || 'somthing went wrong' }));
+};
+module.exports = login;

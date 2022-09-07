b23cdb634043be970bae6ff11a83beaef3d1f547
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-07 23:47:36 +0300

    created add post client side functionality and handled all message and events. relates #30

--- private/homepage/index.js
+++ private/homepage/index.js
@@ -6,6 +6,10 @@ const modeToggle = document.querySelector('header #drop-down .mode-toggle');
 const logout = document.querySelector('header #drop-down #logout');
 const status = document.querySelectorAll('.status label');
 
+const messageHandler = document.querySelector('.message');
+const messagePara = document.querySelector('.message p');
+const messageSpan = document.querySelector('.message span');
+
 main.addEventListener('click', () => {
   dropDownMenu.style.display = 'none';
 });
@@ -48,4 +52,52 @@ modeToggle.addEventListener('click', () => {
 
 logout.addEventListener('click', () => {
   fetch('/logout');
+});
+
+// ============ Create post functions ===================
+
+const layout = document.querySelector('.layout');
+const layer = document.querySelector('.layout .layer');
+const postBtn = document.querySelector('.layout #form #post');
+const postInput = document.querySelector('#add-post #post-input');
+
+postInput.addEventListener('click', () => {
+  layout.style.display = 'flex';
+});
+
+layer.addEventListener('click', () => {
+  layout.style.display = 'none';
+});
+
+postBtn.addEventListener('click', () => {
+  const content = document.querySelector('.layout #form textarea');
+  const postImg = document.querySelector('.layout #form input');
+  if (content.value !== '') {
+    fetch('/addpost', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        content: content.value,
+        postImg: postImg.value,
+      }),
+    }).then((res) => res.json()).then((data) => {
+      if (data.message === 'Created Post Successfuly') {
+        messagePara.textContent = data.message;
+        messageSpan.classList.add('vanishspan');
+        messageHandler.classList.add('vanish');
+        messageHandler.style.backgroundColor = '#1b951b';
+        messageSpan.style.backgroundColor = '#13ff13';
+        setTimeout(() => {
+          messageHandler.classList.remove('vanish');
+          messageSpan.classList.remove('vanishspan');
+        }, 2000);
+        layout.style.display = 'none';
+        content.value = '';
+        postImg.value = '';
+      }
+    });
+    layout.style.display = 'none';
+  } else {
+    content.style.outline = '2px solid red';
+  }
 });

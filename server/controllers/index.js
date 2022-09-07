30a17297153efd7ab103ad0d2d7b547e4a476d36
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-07 20:31:39 +0300

    required delete post function and export this. relates #29

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,8 @@ const signUp = require('./signUp');
 const login = require('./login');
 const getAllPosts = require('./getAllPosts');
 const createPost = require('./createPost');
+const deletePost = require('./deletePost');
 
 module.exports = {
-  signUp, login, getAllPosts, createPost,
+  signUp, login, getAllPosts, createPost, deletePost,
 };

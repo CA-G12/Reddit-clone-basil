ce9f0b5439313a533d54de02a1b5ee1d9b1a859d
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-08 21:52:17 +0300

    created delete post functionality and handle post functioality. relates #39

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -10,6 +10,137 @@ const messageHandler = document.querySelector('.message');
 const messagePara = document.querySelector('.message p');
 const messageSpan = document.querySelector('.message span');
 
+let headerUsername = '';
+
+// ================= Handle posts ======================
+
+fetch('/user').then((res) => res.json()).then((data) => {
+  if (data.userimg) {
+    document.querySelector('#drop #profile-info img').src = data.userimg;
+  } else {
+    document.querySelector('#drop #profile-info img').src = 'https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg';
+  }
+  headerUsername = data.username;
+  document.querySelector('#drop #profile-info p').textContent = data.username;
+});
+
+function deletePost(id) {
+  fetch(`/delete/post/${id}`, {
+    method: 'DELETE',
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.message === 'Post deleted successfuly') {
+        messagePara.textContent = data.message;
+        messageSpan.classList.add('vanishspan');
+        messageHandler.classList.add('vanish');
+        messageHandler.style.width = '350px';
+        messageHandler.style.backgroundColor = '#1b951b';
+        messageSpan.style.backgroundColor = '#13ff13';
+        setTimeout(() => {
+          messageHandler.classList.remove('vanish');
+          messageSpan.classList.remove('vanishspan');
+        }, 2000);
+        createPosts();
+      }
+    });
+}
+
+function handleDom(data, user) {
+  const postsContainer = document.querySelector('#posts');
+  postsContainer.textContent = '';
+  data.forEach((obj) => {
+    const post = document.createElement('div');
+    if (user === obj.username) {
+      const deleteBtn = document.createElement('i');
+      deleteBtn.classList = 'fa-solid fa-xmark';
+      post.appendChild(deleteBtn);
+      deleteBtn.addEventListener('click', () => {
+        deletePost(obj.id);
+      });
+    }
+    post.classList = 'post';
+
+    const voteDiv = document.createElement('div');
+    voteDiv.id = 'vote';
+    const votePlus = document.createElement('i');
+    votePlus.classList = 'fa-solid fa-chevron-up';
+    const voteNumber = document.createElement('p');
+    voteNumber.textContent = obj.likenum;
+    const voteMinus = document.createElement('i');
+    voteMinus.classList = 'fa-solid fa-chevron-down';
+    voteDiv.appendChild(votePlus);
+    voteDiv.appendChild(voteNumber);
+    voteDiv.appendChild(voteMinus);
+    post.appendChild(voteDiv);
+
+    const container = document.createElement('div');
+    container.classList = 'container';
+
+    const userInfoDiv = document.createElement('div');
+    userInfoDiv.classList = 'user-info';
+    const statusDiv = document.createElement('div');
+    statusDiv.classList = 'status';
+    const userImg = document.createElement('img');
+    if (obj.userimg) {
+      userImg.src = obj.userimg;
+      userImg.alt = obj.username;
+    } else {
+      userImg.src = 'https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg';
+      userImg.alt = 'placeholder';
+    }
+    statusDiv.appendChild(userImg);
+    userInfoDiv.appendChild(statusDiv);
+    const postUsername = document.createElement('h3');
+    postUsername.textContent = obj.username;
+    const postDate = document.createElement('p');
+    const postDateTime = obj.postdate.split('T')[0];
+    postDate.textContent = `Posted by ${obj.username} ${postDateTime}`;
+    userInfoDiv.appendChild(postUsername);
+    userInfoDiv.appendChild(postDate);
+    container.appendChild(userInfoDiv);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.classList = 'content';
+    const postContent = document.createElement('p');
+    postContent.textContent = obj.post;
+    contentDiv.appendChild(postContent);
+    container.appendChild(contentDiv);
+    if (obj.postimg) {
+      const postImgDiv = document.createElement('div');
+      postImgDiv.classList = 'img';
+      const postImg = document.createElement('img');
+      postImg.src = obj.postimg;
+      postImg.alt = obj.username;
+      postImgDiv.appendChild(postImg);
+      container.appendChild(postImgDiv);
+    }
+
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.classList = 'buttons';
+    const commentBtn = document.createElement('button');
+    commentBtn.id = 'comment';
+    commentBtn.innerHTML = '<i class="fa-regular fa-message"></i> Comments';
+    const saveBtn = document.createElement('button');
+    saveBtn.id = 'save';
+    saveBtn.innerHTML = '<i class="fa-regular fa-bookmark"></i> Save';
+    buttonsDiv.appendChild(commentBtn);
+    buttonsDiv.appendChild(saveBtn);
+    container.appendChild(buttonsDiv);
+
+    post.appendChild(container);
+    postsContainer.appendChild(post);
+  });
+}
+
+function createPosts() {
+  fetch('/posts').then((res) => res.json()).then((data) => {
+    handleDom(data, headerUsername);
+  });
+}
+
+createPosts();
+
 main.addEventListener('click', () => {
   dropDownMenu.style.display = 'none';
 });
@@ -95,6 +226,7 @@ postBtn.addEventListener('click', () => {
         layout.style.display = 'none';
         content.value = '';
         postImg.value = '';
+        createPosts();
       }
     });
     layout.style.display = 'none';

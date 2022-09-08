98a65c6c25a54c3980ec84713c0214560846e8d8
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 00:50:09 +0300

    required update user data funciton and export this. relates #41

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,7 +5,8 @@ const createPost = require('./createPost');
 const deletePost = require('./deletePost');
 const logOut = require('./logout');
 const userData = require('./userData');
+const updateUserDate = require('./updateUserData');
 
 module.exports = {
-  signUp, login, getAllPosts, createPost, deletePost, logOut, userData,
+  signUp, login, getAllPosts, createPost, deletePost, logOut, userData, updateUserDate,
 };

69e9afd9fefae3063e768b320b3ea46e4befa9cf
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-08 19:33:54 +0300

    edited get post query by get new colum like post image. relates #35

--- server/database/queries/getPostsQuery.js
+++ server/database/queries/getPostsQuery.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
 
-const getPostsQuery = () => connection.query('SELECT COUNT(*) AS likeNum, users.username, users.img_url, posts.post, posts.post_date FROM posts JOIN users ON users.id = posts.user_id LEFT JOIN likes ON posts.id = likes.post_id GROUP BY(likes.post_id, users.username, posts.post, users.img_url, posts.post_date)');
+const getPostsQuery = () => connection.query('SELECT COUNT(*) AS likeNum, posts.id, users.username, users.img_url AS userImg, posts.post, posts.post_date AS postDate, posts.post_img AS postImg FROM posts JOIN users ON users.id = posts.user_id LEFT JOIN likes ON posts.id = likes.post_id GROUP BY(likes.post_id, users.username, posts.post, users.img_url, posts.post_date, posts.id, posts.post_img)');
 
 module.exports = getPostsQuery;

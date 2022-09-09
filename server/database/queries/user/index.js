f29c7a0414d27dbd9b02fc59fa5b403ac3dc92d7
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 17:53:21 +0300

    requierd user data query and export this. relates #51

--- server/database/queries/user/index.js
+++ server/database/queries/user/index.js
@@ -2,7 +2,8 @@ const createPostQuery = require('./createPostQuery');
 const deletePostQuery = require('./deletePostQuery');
 const updateUserDataQuery = require('./updateUserDataQuery');
 const userPostsQuery = require('./userPostsQuery');
+const userDataQuery = require('./userDataQuery');
 
 module.exports = {
-  createPostQuery, deletePostQuery, updateUserDataQuery, userPostsQuery,
+  createPostQuery, deletePostQuery, updateUserDataQuery, userPostsQuery, userDataQuery,
 };

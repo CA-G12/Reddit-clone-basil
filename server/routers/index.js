0dc67b595de53a19ba1358bb28520d54004a6fc1
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 13:15:20 +0300

    created user-posts router endpoint to get all of user posts. relates #47

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const {
-  signUp, login, getAllPosts, createPost, deletePost, logOut, userData, updateUserDate,
+  signUp, login, getAllPosts, createPost, deletePost, logOut, userData, updateUserDate, userPosts,
 } = require('../controllers');
 
 const router = Router();
@@ -9,6 +9,7 @@ router.get('/user', userData);
 router.post('/signup', signUp);
 router.post('/login', login);
 router.get('/posts', getAllPosts);
+router.get('/user-posts', userPosts);
 router.post('/addpost', createPost);
 router.delete('/delete/post/:id', deletePost);
 router.post('/updateuser', updateUserDate);

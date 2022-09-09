5d4dfc4e740b099afd870f52b494e2f043f65e65
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 12:03:06 +0300

    edited update data query by remove the returning value and added new update column. relates #45

--- server/database/queries/updateUserDataQuery.js
+++ server/database/queries/updateUserDataQuery.js
@@ -1,10 +1,7 @@
 const connection = require('../config/connection');
 
 const updateUserDataQuery = ({
-  firstName, lastName, username, email, id,
-}) => {
-  console.log(id);
-  return connection.query('UPDATE users SET first_name = $1, last_name = $2, username = $3, email = $4 WHERE id = $5 RETURNING id', [firstName, lastName, username, email, id]);
-};
+  firstName, lastName, username, email, imgUrl, id,
+}) => connection.query('UPDATE users SET first_name=$1, last_name=$2, username=$3, email=$4, img_url=$5 WHERE id=$6', [firstName, lastName, username, email, imgUrl, id]);
 
 module.exports = updateUserDataQuery;

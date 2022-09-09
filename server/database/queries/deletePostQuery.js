80c6dff86a409eb15be6766b2e2408d417cd8cd8
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 13:10:34 +0300

    removed createpostsquery, deletepostquery, updateuserdataquery fils from queries folder to user folder. relates #47

--- server/database/queries/deletePostQuery.js
+++
@@ -1,5 +0,0 @@
-const connection = require('../config/connection');
-
-const deletePostQuery = (id) => connection.query('DELETE FROM posts WHERE id = $1 RETURNING id', [id]);
-
-module.exports = deletePostQuery;

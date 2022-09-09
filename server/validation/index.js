c95d01bce1984804eefa7f5b0ada5d3aa02bb754
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-09 12:03:54 +0300

    requiered update validation schema and export this. relates #45

--- server/validation/index.js
+++ server/validation/index.js
@@ -1,9 +1,11 @@
 const validate = require('./validator');
 const signUpValidationSchema = require('./signupValidation');
 const loginValidationSchema = require('./loginValidation');
+const updateValidationSchema = require('./updateValidation');
 
 module.exports = {
   validate,
   signUpValidationSchema,
   loginValidationSchema,
+  updateValidationSchema,
 };

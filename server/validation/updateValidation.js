c50a7052faa7ff6d06700c2a42d0e03a042d99d4
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-10 21:38:27 +0300

    edited username validation in signup and update schema. relates #67

--- server/validation/updateValidation.js
+++ server/validation/updateValidation.js
@@ -3,7 +3,8 @@ const joi = require('joi');
 const updateValidationSchema = joi.object({
   firstName: joi.string().required(),
   lastName: joi.string().required(),
-  userName: joi.string().alphanum().min(3).max(30),
+  username: joi.string().alphanum().min(3).max(30)
+    .required(),
   email: joi.string().email().required(),
   imgUrl: joi.string().empty(''),
 });

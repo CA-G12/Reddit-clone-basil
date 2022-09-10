d7b83984d676ea8b20e386689717e022b94af774
Bas-sheikh <basilelshakhe@gmail.com>
2022-09-10 12:34:39 +0300

    created delete, add, get comment router endpoint. relates #57

--- server/routers/index.js
+++ server/routers/index.js
@@ -12,6 +12,9 @@ const {
   userPosts,
   addLike,
   removeLike,
+  addComment,
+  getComments,
+  deleteComment,
 } = require('../controllers');
 
 const router = Router();
@@ -22,10 +25,13 @@ router.post('/login', login);
 router.get('/posts', getAllPosts);
 router.get('/user-posts', userPosts);
 router.post('/addpost', createPost);
-router.delete('/delete/post/:id', deletePost);
+router.delete('/post/:id', deletePost);
 router.post('/updateuser', updateUserDate);
 router.get('/logout', logOut);
 router.post('/like/add/:id', addLike);
 router.delete('/like/down/:id', removeLike);
+router.post('/add/comment', addComment);
+router.get('/comments', getComments);
+router.delete('/comment/:id', deleteComment);
 
 module.exports = router;
